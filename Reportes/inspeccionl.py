e52221a38d8d764799c6ba2472bd6b36f7f336b0
Romario Villalobos <romarioavr@gmail.com>
2024-04-16 20:37:26 -0600

    asfasd

--- Reportes/inspeccionl.py
+++ Reportes/inspeccionl.py
@@ -99,7 +99,7 @@ class InspeccionL(FPDF):
         
 
         
-        locale.setlocale(locale.LC_ALL, 'es_ES')
+        locale.setlocale(locale.LC_ALL, '')
         pdf=FPDF(orientation='P', unit='mm', format='Letter') 
         pdf.add_page()
         
